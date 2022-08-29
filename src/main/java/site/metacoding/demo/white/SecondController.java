58054fbba36e70c74bd5d85edb78db2b82f9a86f
fuckyougit <sopu55544@gmail.com>
2022-08-29 16:16:18 +0900

    컨트롤러 데이터 받기 고급 업데이트

--- src/main/java/site/metacoding/demo/white/SecondController.java
+++ src/main/java/site/metacoding/demo/white/SecondController.java
@@ -15,7 +15,7 @@ public class SecondController {
 // GET -> QueryString -> http body가 없음 -> QueryString. PathVariable -> From(GET) -> 브라우저 (주소 뒤 ? 붙이기)
 // POST.PUT -> http body 담아준다 -> Form(POST) -> JS
 // DELETE -> http body가 없음 -> QueryString. PathVariable(PK)
-	@GetMapping("/second") //PK(id)가 1인것
+	@GetMapping("/second/{id}") //PK(id)가 1인것
 	public String getData(@PathVariable Integer id) {
 		return "id :"+id;
 	}
@@ -23,18 +23,18 @@ public class SecondController {
 	// QueryString = x-www-머시기 타입이다.
 	@GetMapping("/second")
 	public String getData2(String title, String content) {
-		return "title :" +title+", content" +content;
+		return "title :" +title+", content : " +content;
 	}
 	
 	//body에 담아 보낸다.
 	@PostMapping("/second")
 	public String postData(String title, String content) { // 스프링 파싱 기본전략 : x-www-form-???
-		return "title :" +title+", content" +content;
+		return "title :" +title+", content : " +content;
 	}
 	
 	@PutMapping("/second")
 	public String putData(String title, String content) { // 스프링 파싱 기본전략 : x-www-form-???
-		return "title :" +title+", content" +content;
+		return "title :" +title+", content : " +content;
 	}
 	@DeleteMapping("/second/{id}")
 	public String deleteData(@PathVariable Integer id) {
---
+++ src/main/java/site/metacoding/demo/white/ThirdController.java
@@ -0,0 +1,32 @@
+package site.metacoding.demo.white;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import site.metacoding.demo.white.domain.Third;
+
+// 고급 데이터 받기 (json, x-www-form-머시기, text)
+@RestController
+public class ThirdController {
+
+	
+	@PostMapping("/third")
+	public String postData(Third third) {
+		return "id : " + third.getId()+", title : " + third.getTitle() + ", content : " + third.getContent();
+	}
+	
+	// UPDATE third SET title = ?.content = ? WHERE id = ?
+	@PutMapping("/third/{id}")
+	public String putData(@PathVariable Integer id, Third third) {
+		return third.toString();
+	}
+	
+	//json 바디로 받고 싶으면 requestbody
+	@PutMapping("/third/{id}/json")
+	public String putJsonData(@PathVariable Integer id,@RequestBody Third third) {
+		return third.toString();
+	}
+}
