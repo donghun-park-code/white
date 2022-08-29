928f0235927db8c45be95d094b0c14b38afe2ce1
fuckyougit <sopu55544@gmail.com>
2022-08-29 13:23:56 +0900

    스프링부트 컨트롤ㄹ러 데이터 받기 기본

---
+++ src/main/java/site/metacoding/demo/white/SecondController.java
@@ -0,0 +1,43 @@
+package site.metacoding.demo.white;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SecondController {
+
+// 데이터 받기
+// GET -> QueryString -> http body가 없음 -> QueryString. PathVariable -> From(GET) -> 브라우저 (주소 뒤 ? 붙이기)
+// POST.PUT -> http body 담아준다 -> Form(POST) -> JS
+// DELETE -> http body가 없음 -> QueryString. PathVariable(PK)
+	@GetMapping("/second") //PK(id)가 1인것
+	public String getData(@PathVariable Integer id) {
+		return "id :"+id;
+	}
+	
+	// QueryString = x-www-머시기 타입이다.
+	@GetMapping("/second")
+	public String getData2(String title, String content) {
+		return "title :" +title+", content" +content;
+	}
+	
+	//body에 담아 보낸다.
+	@PostMapping("/second")
+	public String postData(String title, String content) { // 스프링 파싱 기본전략 : x-www-form-???
+		return "title :" +title+", content" +content;
+	}
+	
+	@PutMapping("/second")
+	public String putData(String title, String content) { // 스프링 파싱 기본전략 : x-www-form-???
+		return "title :" +title+", content" +content;
+	}
+	@DeleteMapping("/second/{id}")
+	public String deleteData(@PathVariable Integer id) {
+		return id +" delete ok";
+	}
+}
